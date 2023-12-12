b823dc53766af35d9f357d9e643c658019553ed8
송현우 <shw9052@naver.com>
2023-12-12 17:17:04 +0900

    Add login page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,11 +1,101 @@
 import * as S from "./style";
+import { ChangeEvent, useEffect, useState } from "react";
 
-const Auth = () => {
-  return (
-    <div>
-      <S.TS></S.TS>
-    </div>
-  );
+const User = {
+  email: "shw9052@naver.com",
+  pw: "dkssud78@",
 };
 
-export default Auth;
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [pw, setPw] = useState("");
+
+  const [emailValid, setEmailValid] = useState(false);
+  const [pwValid, setPwValid] = useState(false);
+  const [notAllow, setNotAllow] = useState(true);
+
+  const handleEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+    const regex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+    if (regex.test(e.target.value)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
+  };
+
+  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    const regex = new RegExp(
+      /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]).{8,24}$/
+    );
+    setPw(e.target.value);
+    if (regex.test(e.target.value)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
+  };
+
+  const onclickConfirmButton = () => {
+    if (email === User.email && pw === User.pw) {
+      alert("로그인에 성공하셨습니다.");
+    } else {
+      alert("등록되지 않은 회원입니다.");
+    }
+  };
+
+  useEffect(() => {
+    if (emailValid && pwValid) {
+      setNotAllow(false);
+      return;
+    }
+    setNotAllow(true);
+  }, [emailValid, pwValid]);
+
+  return (
+    <S.Page>
+      <S.TitleWrap>
+        이메일과 비밀번호를
+        <br />
+        입력해주세요
+      </S.TitleWrap>
+      <S.ContentWrap>
+        <S.InputTitle>이메일 주소</S.InputTitle>
+        <S.InputWrap>
+          <S.Input
+            placeholder="text@gmail.com"
+            type="text"
+            value={email}
+            onChange={handleEmail}
+          />
+        </S.InputWrap>
+        <S.ErrorMessageWrap>
+          {!emailValid && email.length > 0 && (
+            <div>올바른 이메일을 입력해주세요.</div>
+          )}
+        </S.ErrorMessageWrap>
+
+        <S.InputTitle style={{ marginTop: "26px" }}>비밀번호</S.InputTitle>
+        <S.InputWrap>
+          <S.Input
+            placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+            type="password"
+            value={pw}
+            onChange={handlePassword}
+          />
+        </S.InputWrap>
+        <S.ErrorMessageWrap>
+          {!pwValid && pw.length > 0 && (
+            <div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+          )}
+        </S.ErrorMessageWrap>
+      </S.ContentWrap>
+
+      <div>
+        <S.BottomButton onClick={onclickConfirmButton} disabled={notAllow}>
+          확인
+        </S.BottomButton>
+      </div>
+    </S.Page>
+  );
+}
--- src/pages/auth/style.ts
+++ src/pages/auth/style.ts
@@ -1,7 +1,85 @@
 import styled from "styled-components";
 
-export const TS = styled.div`
+export const Page = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
   width: 100%;
-  height: 100px;
-  background-color: red;
+  max-width: 31.25rem;
+  padding: 0 1.25rem;
+  left: 50%;
+  transform: translate(-50%, 0);
+  background-color: #f7f7f7;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const TitleWrap = styled.div`
+  margin-top: 5.4375rem;
+  font-size: 1.625rem;
+  font-weight: 700;
+  color: #262626;
+`;
+
+export const ContentWrap = styled.div`
+  margin-top: 1.625rem;
+  flex: 1;
+`;
+
+export const InputTitle = styled.div`
+  font-size: 0.75rem;
+  font-weight: 600;
+  color: #262626;
+`;
+
+export const InputWrap = styled.div`
+  display: flex;
+  border-radius: 0.5rem;
+  padding-left: 1rem;
+  margin-top: 0.5rem;
+  background-color: #fff;
+  border: 0.0625rem solid #e2e0e0;
+  &:focus-within {
+    border: 0.0625rem solid #9e30f4;
+  }
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  outline: none;
+  border: none;
+  height: 1.0625rem;
+  font-size: 0.875rem;
+  font-weight: 400;
+  margin: 0.9375rem;
+  margin-left: 0;
+  &::placeholder {
+    color: #dadada;
+  }
+`;
+
+export const ErrorMessageWrap = styled.div`
+  margin-top: 0.5rem;
+  color: #ef0000;
+  font-size: 0.75rem;
+`;
+
+export const BottomButton = styled.button`
+  width: 100%;
+  height: 3rem;
+  border: none;
+  font-weight: 700;
+  background-color: #9e30f4;
+  border-radius: 4rem;
+  color: #fff;
+  margin-bottom: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  &:disabled {
+    background-color: #dadada;
+    color: #fff;
+  }
 `;
