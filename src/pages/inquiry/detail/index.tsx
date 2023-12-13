a9250f4b24e751f80729e2e24c2a8e13fcdbd465
송현우 <shw9052@naver.com>
2023-12-13 19:07:31 +0900

    Add detail page

--- src/pages/inquiry/detail/index.tsx
+++ src/pages/inquiry/detail/index.tsx
@@ -1,21 +1,56 @@
 import * as S from "./style";
+import { useNavigate } from "react-router-dom";
 
 const Detail = () => {
+  const navigate = useNavigate();
+
+  function notAccess() {
+    alert("문의가 거부 되었습니다.");
+  }
+
+  function access() {
+    alert("문의가 채택 되었습니다.");
+  }
+
   return (
     <S.InquiryWrapper>
+      <S.BackText onClick={() => navigate("/")}>{"< 돌아가기"}</S.BackText>
       <S.InquiryContainer>
         <S.InquiryItem>
           <S.InquiryItemInner>
             <S.TextBox>
               <S.TitleText>유색 페트병이 등록되어 있지 않습니다</S.TitleText>
-              <S.TypeText>검토 중</S.TypeText>
-              <S.DateText>23.12.10</S.DateText>
+              <S.SemiTextBox>
+                <S.DateText>23.12.10</S.DateText>
+                <S.TypeText>검토 중</S.TypeText>
+              </S.SemiTextBox>
             </S.TextBox>
-            <S.ImgBox
-              style={{
-                backgroundImage: `url('https://opgg-com-image.akamaized.net/attach/images/20200420133621.1102639.jpg')`,
-              }}
-            />
+            <S.MainBox>
+              <S.ImgBox
+                style={{
+                  backgroundImage: `url('https://opgg-com-image.akamaized.net/attach/images/20200420133621.1102639.jpg')`,
+                }}
+              />
+              <S.MainTextBox>
+                <div>
+                  <S.ValueTitle>문의내용</S.ValueTitle>
+                  <S.MainText>
+                    와~ 희철이! 내 동생 도희철이~ 와 이라십니까 형님? 니가 직접
+                    가라! 응? 니가 가서 우리 2공수 아들 모조리 델고 온나.
+                    뭐랍니까? 웃어? 이 새끼 봐라… 잠깐만 잠깐만, 야 야 야
+                    어어어! 놔 봐라 쫌! 안 쏜다! 야 도희철 일루 와, 앞으로 와 이
+                    스끼야! 총 잡아. 총 잡아! 니가 가기 싫으모 내 심장에다가 팍
+                    쏴 삐라. 쏘라고!
+                  </S.MainText>
+                </div>
+                <S.BtnContainer>
+                  <S.NotAccessBtn onClick={() => notAccess()}>
+                    거부하기
+                  </S.NotAccessBtn>
+                  <S.AccessBtn onClick={() => access()}>채택하기</S.AccessBtn>
+                </S.BtnContainer>
+              </S.MainTextBox>
+            </S.MainBox>
           </S.InquiryItemInner>
         </S.InquiryItem>
       </S.InquiryContainer>
--- src/pages/inquiry/detail/style.ts
+++ src/pages/inquiry/detail/style.ts
@@ -6,31 +6,28 @@ export const InquiryWrapper = styled.div`
   padding-top: 10rem;
 `;
 
+export const BackText = styled.span`
+  position: absolute;
+  left: 23.5rem;
+  top: 5rem;
+  font-size: 1.25rem;
+  cursor: pointer;
+  color: #3484db;
+`;
+
 export const InquiryContainer = styled.div`
   width: 75rem;
   height: 100%;
 `;
 
 export const InquiryItem = styled.div`
-  border-bottom: solid 1px #e2e2e2;
-  cursor: pointer;
   display: flex;
   align-items: center;
-  height: 7rem;
 `;
 
 export const InquiryItemInner = styled.div`
-  height: 5rem;
   display: flex;
-`;
-
-export const ImgBox = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-  margin-left: 1rem;
+  flex-direction: column;
 `;
 
 export const TextBox = styled.div`
@@ -39,21 +36,97 @@ export const TextBox = styled.div`
   margin-left: 1rem;
 `;
 
+export const TitleText = styled.span`
+  font-size: 2rem;
+  display: flex;
+`;
+
+export const SemiTextBox = styled.div`
+  display: flex;
+  margin-top: 0.5rem;
+`;
+
+export const DateText = styled.span`
+  font-size: 1.25rem;
+  display: flex;
+  color: #595959;
+  margin-right: 1rem;
+`;
+
 export const TypeText = styled.span`
-  margin-bottom: 0.45rem;
-  font-size: 1rem;
+  font-size: 1.25rem;
   display: flex;
   color: #bfbfbf;
 `;
 
-export const TitleText = styled.span`
-  font-size: 1.5rem;
+export const MainBox = styled.div`
+  margin-top: 1rem;
+  padding-left: 1rem;
+  width: 75rem;
   display: flex;
 `;
 
-export const DateText = styled.span`
-  margin-top: 0.25rem;
-  font-size: 1.25rem;
+export const ImgBox = styled.div`
+  width: 30rem;
+  height: 30rem;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const MainTextBox = styled.div`
   display: flex;
-  color: #595959;
+  width: 45rem;
+  margin-left: 1rem;
+  flex-wrap: wrap;
+`;
+
+export const ValueTitle = styled.span`
+  font-size: 2rem;
+`;
+
+export const MainText = styled.div`
+  margin-top: 1rem;
+  font-size: 1rem;
+`;
+
+export const BtnContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 45rem;
+  align-items: flex-end;
+  div {
+    display: flex;
+    align-items: center;
+    margin: 0 2.5rem;
+    justify-content: center;
+    border-radius: 0.5rem;
+    color: #fff;
+    width: 11.25rem;
+    height: 3.25rem;
+    font-size: 1.5rem;
+    cursor: pointer;
+  }
+`;
+
+export const NotAccessBtn = styled.div`
+  background-color: #df454a;
+  &:hover {
+    background-color: #f5f5f5;
+    width: 11rem;
+    height: 3rem;
+    border: solid 0.125rem #df454a;
+    color: #df454a;
+  }
+`;
+
+export const AccessBtn = styled.div`
+  background-color: #288be1;
+  &:hover {
+    background-color: #f5f5f5;
+    width: 11rem;
+    height: 3rem;
+    border: solid 0.125rem #288be1;
+    color: #288be1;
+  }
 `;
