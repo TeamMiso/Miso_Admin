6d179b974fd642dd0710aa299feaa7aa9edd7fbf
송현우 <shw9052@naver.com>
2023-12-20 02:41:07 +0900

    Update inquiry detail design publishing

--- src/pages/inquiry/detail/index.tsx
+++ src/pages/inquiry/detail/index.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useEffect, useState } from "react";
 import * as S from "./style";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Header } from "../../../components";
 
 const Detail = () => {
   const navigate = useNavigate();
@@ -86,70 +87,61 @@ const Detail = () => {
 
   return (
     <S.InquiryWrapper>
+      <Header />
       <S.InquiryContainer>
-        <S.BackText onClick={() => navigate("/")}>
-          {"< 돌아가기"}
-        </S.BackText>
-        <S.InquiryItem>
-          <S.InquiryItemInner>
-            <S.TextBox>
-              <S.TitleText>{title}</S.TitleText>
-              <S.SemiTextBox>
-                <S.DateText>
-                  {`${inquiryDate.slice(0, 4)}년 ${inquiryDate.slice(
-                    5,
-                    7
-                  )}월 ${inquiryDate.slice(8, 10)}일 ${inquiryDate.slice(
-                    11,
-                    16
-                  )}`}
-                </S.DateText>
-                <S.TypeText
-                  style={{
-                    color: inquiryStatus === "WAIT" ? "#BFBFBF" : "#25D07D",
-                  }}
-                >
-                  {inquiryStatus === "WAIT" ? "검토 중" : "답변 완료"}
-                </S.TypeText>
-              </S.SemiTextBox>
-            </S.TextBox>
-            <S.MainBox>
-              <S.ImgBox
+        <S.InquiryBox>
+          <S.BackText onClick={() => navigate("/")}>{"< 돌아가기"}</S.BackText>
+          <S.TitleBox>
+            <S.MainTitle>{title}</S.MainTitle>
+            <S.SemiTitleBox>
+              <span>
+                {`${inquiryDate.slice(2, 4)}.${inquiryDate.slice(
+                  5,
+                  7
+                )}.${inquiryDate.slice(8, 10)}`}
+              </span>
+              <span
                 style={{
-                  backgroundImage: `url(${imageUrl})`,
+                  color: inquiryStatus === "WAIT" ? "#BFBFBF" : "#25D07D",
                 }}
-              />
-              <S.MainTextBox>
-                <div>
-                  <S.ValueTitle>문의내용</S.ValueTitle>
-                  <S.MainText>{content}</S.MainText>
-                  <S.ValueTitle>문의답변</S.ValueTitle>
-                  <S.AnswerBox
-                    style={{
-                      display: inquiryStatus === "WAIT" ? "flex" : "none",
-                    }}
-                    placeholder="문의 내용 입력"
-                    onChange={handleText}
-                  />
-                  <S.AnswerText
-                    style={{
-                      display: inquiryStatus === "WAIT" ? "none" : "flex",
-                    }}
-                  >
-                    {inquiryText}
-                  </S.AnswerText>
-                </div>
-                <S.BtnContainer
-                  style={{
-                    display: inquiryStatus === "WAIT" ? "flex" : "none",
-                  }}
-                >
-                  <S.AccessBtn onClick={() => access()}>답변하기</S.AccessBtn>
-                </S.BtnContainer>
-              </S.MainTextBox>
-            </S.MainBox>
-          </S.InquiryItemInner>
-        </S.InquiryItem>
+              >
+                {inquiryStatus === "WAIT" ? "검토 중" : "답변 완료"}
+              </span>
+            </S.SemiTitleBox>
+          </S.TitleBox>
+          <S.MainDocumentWrapper>
+            <S.MainText>{content}</S.MainText>
+            <S.ImgBox
+              style={{
+                backgroundImage: `url(${imageUrl})`,
+              }}
+            />
+          </S.MainDocumentWrapper>
+          <S.AnswerContainer>
+            <span>답변내용</span>
+            <S.AnswerBox
+              style={{
+                display: inquiryStatus === "WAIT" ? "flex" : "none",
+              }}
+              placeholder="답변 내용 쓰기"
+              onChange={handleText}
+            />
+          </S.AnswerContainer>
+          <S.AnswerText
+            style={{
+              display: inquiryStatus === "WAIT" ? "none" : "flex",
+            }}
+          >
+            {inquiryText}
+          </S.AnswerText>
+        </S.InquiryBox>
+        <S.ButtonContainer
+          style={{
+            display: inquiryStatus === "WAIT" ? "flex" : "none",
+          }}
+        >
+          <S.AccessButton onClick={() => access()}>답변하기</S.AccessButton>
+        </S.ButtonContainer>
       </S.InquiryContainer>
     </S.InquiryWrapper>
   );
--- src/pages/inquiry/detail/style.ts
+++ src/pages/inquiry/detail/style.ts
@@ -3,131 +3,129 @@ import styled from "styled-components";
 export const InquiryWrapper = styled.div`
   display: flex;
   justify-content: center;
-  padding-top: 5rem;
+  height: 100vh;
 `;
 
 export const InquiryContainer = styled.div`
   width: 75rem;
   height: 100%;
+  background-color: #fff;
+  border-left: solid 1px #bfbfbf;
+  border-right: solid 1px #bfbfbf;
+`;
+
+export const InquiryBox = styled.div`
+  margin: 0 2.5rem;
+  margin-top: 7rem;
 `;
 
 export const BackText = styled.span`
   font-size: 1.25rem;
-  margin-left: 1.25rem;
   cursor: pointer;
   color: #3484db;
 `;
 
-export const InquiryItem = styled.div`
-  display: flex;
+export const TitleBox = styled.div`
   margin-top: 1rem;
-  align-items: center;
 `;
 
-export const InquiryItemInner = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-export const TextBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-left: 1rem;
-`;
-
-export const TitleText = styled.span`
+export const MainTitle = styled.span`
   font-size: 2rem;
+  font-weight: 700;
+  line-height: 3rem;
+`;
+
+export const SemiTitleBox = styled.div`
   display: flex;
-`;
-
-export const SemiTextBox = styled.div`
-  display: flex;
-  margin-top: 0.5rem;
-`;
-
-export const DateText = styled.span`
-  font-size: 1.25rem;
-  display: flex;
-  color: #595959;
-  margin-right: 1rem;
-`;
-
-export const TypeText = styled.span`
-  font-size: 1.25rem;
-  display: flex;
-`;
-
-export const MainBox = styled.div`
-  margin-top: 1rem;
-  padding-left: 1rem;
-  width: 75rem;
-  display: flex;
-`;
-
-export const ImgBox = styled.div`
-  width: 30rem;
-  height: 30rem;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-`;
-
-export const MainTextBox = styled.div`
-  display: flex;
-  width: 45rem;
-  margin-left: 1rem;
-  flex-wrap: wrap;
-`;
-
-export const ValueTitle = styled.span`
-  font-size: 2rem;
-`;
-
-export const MainText = styled.div`
-  margin-top: 1rem;
-  font-size: 1rem;
-  height: 12rem;
-  width: 45rem;
-`;
-
-export const AnswerBox = styled.textarea`
-  width: 44.5rem;
-  margin-top: 1rem;
-  height: 8.25rem;
-  resize: none;
-`;
-
-export const AnswerText = styled.div`
-  margin-top: 1rem;
-  font-size: 1rem;
-`
-
-export const BtnContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 45rem;
-  align-items: flex-end;
-  div {
-    display: flex;
-    align-items: center;
-    margin: 0 2.5rem;
-    justify-content: center;
-    border-radius: 0.5rem;
-    color: #fff;
-    width: 11.25rem;
-    height: 3.25rem;
+  span {
     font-size: 1.5rem;
-    cursor: pointer;
+    margin-right: 0.5rem;
+    font-weight: 400;
+    line-height: 2rem;
   }
 `;
 
-export const AccessBtn = styled.div`
-  background-color: #288be1;
+export const MainDocumentWrapper = styled.div`
+  margin-top: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: solid 1px #bfbfbf;
+`;
+
+export const MainText = styled.div`
+  width: 42.75rem;
+  margin-bottom: 1.5rem;
+  font-size: 1.25rem;
+  font-weight: 500;
+  line-height: 2rem;
+`;
+
+export const ImgBox = styled.div`
+  width: 26.25rem;
+  height: 17.5rem;
+  margin-bottom: 1.5rem;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 0.5rem;
+`;
+
+export const AnswerContainer = styled.div`
+  margin-top: 1.5rem;
+  span {
+    font-size: 1.625rem;
+    font-weight: 700;
+    line-height: 2.5rem;
+  }
+`;
+
+export const AnswerBox = styled.textarea`
+  resize: none;
+  width: 100%;
+  height: 100px;
+  border: none;
+  font-size: 1.25rem;
+  line-height: 2rem;
+  font-weight: 400;
+  cursor: text;
+  &:placeholder-shown {
+    font-size: 1.25rem;
+    font-weight: 700;
+    line-height: 2rem;
+  }
+`;
+
+export const AnswerText = styled.div`
+  font-size: 1.25rem;
+  line-height: 2rem;
+  font-weight: 400;
+`;
+
+export const ButtonContainer = styled.div`
+  position: fixed;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  left: 0;
+`;
+
+export const AccessButton = styled.div`
+  background-color: #25d07d;
+  color: #fff;
+  font-size: 1rem;
+  font-weight: 700;
+  width: 22.5rem;
+  height: 3rem;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   &:hover {
-    background-color: #f5f5f5;
-    width: 11rem;
-    height: 3rem;
-    border: solid 0.125rem #288be1;
-    color: #288be1;
+    background-color: #fff;
+    border: solid 0.125rem #25d07d;
+    color: #25d07d;
+    width: 22.375rem;
+    height: 2.875rem;
   }
 `;
